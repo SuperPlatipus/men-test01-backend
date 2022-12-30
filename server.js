12948cf3f0d51428d2ef1a6b798af5d35cf7cdc0
SuperPlatipus <119877319+SuperPlatipus@users.noreply.github.com>
2022-12-30 11:45:07 -0400

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+
+const cors = require('cors')
+
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
@@ -9,6 +12,8 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 // middleware
+
+app.use(cors())
 
 app.use(express.json())
 
