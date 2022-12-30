1c79406a78d8ee26f4ca3030c3d7a58f4c771b91
SuperPlatipus <119877319+SuperPlatipus@users.noreply.github.com>
2022-12-30 10:00:57 -0400

    backend ready to start

---
+++ routes/workouts.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
+
+
+const router = express.Router()
+
+// require auth for all workout routes
+
+router.use(requireAuth)
+
+// Get all workouts
+
+router.get('/', getWorkouts)
+
+router.get('/:id', getWorkout)
+
+router.post('/', createWorkout)
+
+router.delete('/:id', deleteWorkout)
+
+router.patch('/:id', updateWorkout)
+
+module.exports = router
